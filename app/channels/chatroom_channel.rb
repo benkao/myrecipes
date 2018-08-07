74768b4e242809e2723125745ef03212d1406995
benkao <kaoben@hotmail.com>
2018-08-07 05:06:34 +0000

    fix chatroom channel

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,6 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-     stream_from "chatroom" #step 1: add channel, next step update form submisson at chatrooms/show.html.erb to take advantage of Ajax
+     stream_from "chatroom_channel" #step 1: add channel, next step update form submisson at chatrooms/show.html.erb to take advantage of Ajax
   end
 
   def unsubscribed
